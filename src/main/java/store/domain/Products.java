94465543d765481e61b127caf707806ea40a262e
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-11-11 16:32:25 +0900

    refactor(domain): 프로모션인 상품에 한하여 일반 재고가 없을 경우 재고없음을 보여주는 기능 수정
    Products.java: 기존의 데이터만 보여주는 기능에서, 프로모션 상품일 경우, 일반 재고가 없을 경우 재고 없음으로 보여줘야하기 위해 관련 사항 리팩터링

--- src/main/java/store/domain/Products.java
+++ src/main/java/store/domain/Products.java
@@ -1,12 +1,14 @@
 package store.domain;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import store.domain.vo.PromotionType;
 
 public class Products {
 
+    private static final int ZERO_PRICE = 0;
+    private static final int PRICE_INDEX = 1;
     private static final int QUANTITY_INDEX = 2;
 
     private final List<Product> products;
@@ -15,39 +17,63 @@ public class Products {
         this.products = makeProducts(contents, promotions);
     }
 
-    public int productCountByName(String name) {
-        List<Product> sameNameProducts = productsByName(name);
-
-        return sameNameProducts.stream()
-                .mapToInt(Product::quantity)
-                .sum();
-    }
-
-    private List<Product> productsByName(String name) {
-        List<Product> sameNameProducts = new ArrayList<>();
-
-        for (Product product : products) {
-            if (product.name().equals(name)) {
-                sameNameProducts.add(product);
-            }
-        }
-
-        return sameNameProducts;
-    }
-
-    public List<Product> products() {
-        return Collections.unmodifiableList(products);
-    }
-
     private List<Product> makeProducts(List<List<String>> contents, Promotions promotions) {
         List<Product> products = new ArrayList<>();
 
         for (List<String> content : contents) {
             Promotion promotion = promotions.getPromotionByType(PromotionType.from(content.getLast()));
-            Product product = new Product(content.getFirst(), content.get(1), content.get(QUANTITY_INDEX), promotion);
+            Product product = new Product(content.getFirst(), content.get(PRICE_INDEX), content.get(QUANTITY_INDEX), promotion);
             products.add(product);
         }
 
         return products;
+    }
+
+    public List<Product> getNoPromotionProducts(String name) {
+        return getProductsByName(name).stream()
+                .filter(product -> !product.isPromotion())
+                .collect(Collectors.toList());
+    }
+
+    public List<Product> getPromotionProducts(String name) {
+        return getProductsByName(name).stream()
+                .filter(Product::isPromotion)
+                .collect(Collectors.toList());
+    }
+
+    public int countProductsByName(String name) {
+        return getProductsByName(name).stream()
+                .mapToInt(Product::getQuantity)
+                .sum();
+    }
+
+    public List<Product> displayProducts() {
+        List<Product> displayedProducts = new ArrayList<>();
+        for (Product product : products) {
+            displayedProducts.add(product);
+            getNoPromotionProducts(product, displayedProducts);
+        }
+        return displayedProducts;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    private List<Product> getProductsByName(String name) {
+        return products.stream()
+                .filter(product -> product.getName().equals(name))
+                .toList();
+    }
+
+    private void getNoPromotionProducts(Product product, List<Product> displayedProducts) {
+        List<Product> noPromotionProducts = getNoPromotionProducts(product.getName());
+        if (product.isPromotion() && noPromotionProducts.isEmpty()) {
+            displayedProducts.add(new Product(
+                    product.getName(),
+                    String.valueOf(product.getPrice()),
+                    String.valueOf(ZERO_PRICE),
+                    Promotion.noPromotion()));
+        }
     }
 }
--- src/test/java/store/domain/ProductsTest.java
+++ src/test/java/store/domain/ProductsTest.java
@@ -5,21 +5,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import store.domain.vo.PromotionType;
 
 @SuppressWarnings("NonAsciiCharacters")
 class ProductsTest {
 
-    private List<List<String>> productsData;
-    private Promotions promotions;
+    private Products products;
 
     @BeforeEach
     void setUp() {
         LocalDateTime fixedLocalDateTime = LocalDate.parse("2024-11-07").atStartOfDay();
-        productsData = List.of(
+        List<List<String>> productsData = List.of(
                 List.of("콜라", "1000", "10", "탄산2+1"),
                 List.of("콜라", "1000", "10", "null"),
                 List.of("빵", "1000", "10", "null"),
@@ -27,38 +30,89 @@ class ProductsTest {
                 List.of("껌", "1000", "7", "null"),
                 List.of("오렌지주스", "1800", "9", "MD추천상품")
         );
-        promotions = new Promotions(
+        Promotions promotions = new Promotions(
                 List.of(
                         List.of("탄산2+1", "2", "1", "2024-01-01", "2024-12-31"),
                         List.of("MD추천상품", "1", "1", "2024-01-01", "2024-12-31"),
-                        List.of("null", "0", "0", "2024-01-01", "2024-12-31")
+                        List.of("null", "0", "0", "2024-01-01", "2024-10-31")
                 ),
                 fixedLocalDateTime
         );
+        products = new Products(productsData, promotions);
     }
 
     @Test
     void 파일_데이터로_객체를_생성한다() {
-        // given
-        Products products = new Products(productsData, promotions);
+        // when
+        List<Product> actual = products.getProducts();
 
         // when
-        List<Product> actual = products.products();
-
-        // when
-        assertThat(actual.size()).isEqualTo(5);
+        assertThat(actual.size()).isEqualTo(6);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"콜라, 20", "사이다, 8","오렌지주스, 9"})
-    void 상품_이름에_맞는_재고_개수를_가져온다(String name, int quantity) {
-        // given
-        Products products = new Products(productsData, promotions);
-
+    void 특정_상품의_재고_개수를_가져온다(String name, int quantity) {
         // when
-        int actual = products.productCountByName(name);
+        int actual = products.countProductsByName(name);
 
         // then
         assertThat(actual).isEqualTo(quantity);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"콜라, 1", "사이다, 1"})
+    void 프로모션에_해당되는_상품만_추출한다(String name, int expectedCount) {
+        // when
+        List<Product> promotionProducts = products.getPromotionProducts(name);
+
+        // then
+        assertThat(promotionProducts)
+                .hasSize(expectedCount)
+                .allMatch(Product::isPromotion);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"콜라, 1", "사이다, 0"})
+    void 프로모션에_해당되지_않는_상품만_추출한다(String name, int expectedCount) {
+        // when
+        List<Product> promotionProducts = products.getNoPromotionProducts(name);
+
+        // then
+        assertThat(promotionProducts)
+                .hasSize(expectedCount)
+                .allMatch(product -> !product.isPromotion());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideShowProducts")
+    void 상품을_보여주기_위해_재고없는_상품을_표시한다(List<Product> productData) {
+        // when
+        List<Product> showProduct = products.displayProducts();
+
+        // then
+        assertThat(showProduct)
+                .containsAll(productData);
+    }
+
+    static Stream<Arguments> provideShowProducts() {
+        Promotions promotions = new Promotions(List.of(
+                List.of("탄산2+1", "2", "1", "2024-01-01", "2024-12-31"),
+                List.of("MD추천상품", "1", "1", "2024-01-01", "2024-12-31")
+        ), LocalDateTime.now());
+        return Stream.of(
+                Arguments.of(
+                        List.of(
+                                new Product("콜라", "1000", "10", promotions.getPromotionByType(PromotionType.from("탄산2+1"))),
+                                new Product("콜라", "1000", "10", Promotion.noPromotion()),
+                                new Product("빵", "1000", "10", Promotion.noPromotion()),
+                                new Product("사이다", "1000", "8", promotions.getPromotionByType(PromotionType.from("탄산2+1"))),
+                                new Product("사이다", "1000", "0", Promotion.noPromotion()),
+                                new Product("껌", "1000", "7", Promotion.noPromotion()),
+                                new Product("오렌지주스", "1800", "9", promotions.getPromotionByType(PromotionType.from("MD추천상품"))),
+                                new Product("오렌지주스", "1800", "0", Promotion.noPromotion())
+                        )
+                )
+        );
     }
 }
